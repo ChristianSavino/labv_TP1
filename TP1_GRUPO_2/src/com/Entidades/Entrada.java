9c2daa28930a5f5e0fdd6289c38787350984fdc2
Christian Savino <christiansavino1811@gmail.com>
2022-03-22 22:22:28 -0300

    Update Varios
    Commit de prueba

--- TP1_GRUPO_2/src/com/Entidades/Entrada.java
+++ TP1_GRUPO_2/src/com/Entidades/Entrada.java
@@ -1,5 +1,27 @@
 package com.Entidades;
 
 public class Entrada {
-
+	protected Float precio;
+	protected String nombre;
+	
+	public Entrada(String n,Float p)
+	{
+		nombre = n;
+		precio = p;
+	}
+	
+	public Float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Float precio) {
+		this.precio = precio;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
 }
+
