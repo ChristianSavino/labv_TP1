42458e0c1f9c087c28152bc6eeb497031c885197
JuanIgnacioBellavitis <juan.bellavitis@gmail.com>
2022-03-22 22:30:52 -0300

    Update MainClass.java
    Prueba juani

--- TP1_GRUPO_2/src/com/principal/MainClass.java
+++ TP1_GRUPO_2/src/com/principal/MainClass.java
@@ -4,6 +4,7 @@ public class MainClass {
 	//comentario de prueba de gihub
 	public static void main(String[] args) {
 		System.out.println("TP1_GRUPO_2");
+		System.out.println("prueba juani");
 	}
 
 }
