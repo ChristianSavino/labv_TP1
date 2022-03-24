d1c597d9100c9dc404d62644ad77fda4ad50a972
Fernando Leonel Moschen <ferchumoschen@outlook.com>
2022-03-24 19:11:15 -0300

    Agregar evento a infantil

--- TP1_GRUPO_2/src/com/Negocio/Infantil.java
+++ TP1_GRUPO_2/src/com/Negocio/Infantil.java
@@ -2,13 +2,19 @@ package com.Negocio;
 import com.Entidades.*;
 
 public class Infantil extends Evento {
-	public static float precioMayor = 500;
-	public static float precioMenor= 250;
-	/*Mayor de 8 años*/
-	
-	public Infantil() {
-	entrada = new Entrada[2];
-	entrada[0] = new Entrada("Mayor",precioMayor);
-	entrada[1] = new Entrada("Menor",precioMenor);
+@Override
+	protected void Constructor(String nombreEvento, String fechaEvento, String localidadEvento) {
+		nombre = nombreEvento;
+		fecha = fechaEvento;
+		localidad = localidadEvento;
 	}
+		public static float precioMayor = 500;
+		public static float precioMenor= 250;
+		/*Mayor de 8 años*/
+	
+		public Infantil() {
+		entrada = new Entrada[2];
+		entrada[0] = new Entrada("Mayor",precioMayor);
+		entrada[1] = new Entrada("Menor",precioMenor);
+		}
 }
